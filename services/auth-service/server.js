ef543f1d17ceb5729da31245ea34d89c5e5f1b11
marcos sandoval <marcosbancaprepa@gmail.com>
2025-09-24 15:57:49 -0600

    cambio en la politica de tokens a lax

--- services/auth-service/server.js
+++ services/auth-service/server.js
@@ -613,7 +613,7 @@ app.post('/login', async (req, res) => {
         res.cookie('refreshToken', refreshToken, {
             httpOnly: true,
             secure: process.env.NODE_ENV === 'production',
-            sameSite: 'strict',
+            sameSite: 'lax',
             path: '/api/auth/refresh-token', // La cookie solo se envía a este endpoint
             maxAge: 7 * 24 * 60 * 60 * 1000 // 7 días
         });
